e04b080eb19b67477608c5c9d7d888070bf1b359
ElegantGracie <gracenwafor50@gmail.com>
2022-04-27 19:22:45 +0100

    commented on work

--- counter.js
+++ counter.js
@@ -1,29 +1,41 @@
+// Buttons Selection 
 document.querySelector('.decrease').addEventListener('click', de);
 document.querySelector('.increase').addEventListener('click', inc);
 document.querySelector('.reset').addEventListener('click', re);
 
+// Decrease Function 
 function de () {
-    document.querySelector('p').innerHTML--;
-    document.querySelector('p').style.color = "rgba(153, 56, 72, 0.671)";
+  document.querySelector('p').innerHTML--;
+  document.querySelector('p').style.color = "rgba(153, 56, 72, 0.671)";
 }
 
+// Increase Function  
 function inc () {
-    document.querySelector('p').innerHTML++;
-    document.querySelector('p').style.color = "rgba(81, 81, 216, 0.568)";
+  document.querySelector('p').innerHTML++;
+  document.querySelector('p').style.color = "rgba(81, 81, 216, 0.568)";
 }
 
+// Reset Function 
 function re () {
-    document.querySelector('p').innerHTML = 0;
-    document.querySelector('p').style.color = "#222";
+  document.querySelector('p').innerHTML = 0;
+  document.querySelector('p').style.color = "#222";
 }
 
-    if (count > 0) {
-      value.style.color = "rgba(81, 81, 216, 0.568)";
-    }
-    if (count < 0) {
-      value.style.color = "rgba(153, 56, 72, 0.671)";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-    }
-    value.innerHTML = count;
+// Color Changes
+// When Increased  
+if (count > 0) {
+  value.style.color = "rgba(81, 81, 216, 0.568)";
+}
+
+// When Reduced 
+if (count < 0) {
+  value.style.color = "rgba(153, 56, 72, 0.671)";
+}
+
+// When Reset 
+if (count === 0) {
+  value.style.color = "#222";
+}
+
+// To output to HTML 
+value.innerHTML = count;
